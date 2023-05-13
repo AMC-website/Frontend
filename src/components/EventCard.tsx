baee388e07f7e7b8a1ee9a64c0c0c0e77807decc
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2023-05-13 23:12:04 +0530

    fixed linting error

--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,36 +1,37 @@
-import { Box, Typography } from "@mui/material"
-import {useTheme} from "@mui/material/styles"
-import styles from "@/styles/events.module.css"
+import { Box, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import styles from '@/styles/events.module.css';
 
-
-export default function EventCard(){
+export default function EventCard() {
     const theme = useTheme();
-    return(
-        <Box
-        
-        >
-        <div
-        className={styles.card}
-        >
-            <img
-            src="/image.jpg"
-            className={styles.card_img}
-            >
-            </img>
-            <div
-           className={styles.cardItems}
-            >
-                <Typography variant="h3" padding="20px 0" fontSize="30px" textAlign="center" >Title</Typography>
-                <p
-                 className={styles.card_content}
-                >Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    return (
+        <Box>
+            <div className={styles.card}>
+                <img src="/image.jpg" className={styles.card_img}></img>
+                <div className={styles.cardItems}>
+                    <Typography
+                        variant="h3"
+                        padding="20px 0"
+                        fontSize="30px"
+                        textAlign="center"
+                    >
+                        Title
+                    </Typography>
+                    <p className={styles.card_content}>
+                        Lorem Ipsum is simply dummy text of the printing and
+                        typesetting industry. Lorem Ipsum has been the
+                        industry&apos;s standard dummy text ever since the
+                        1500s, when an unknown printer took a galley of type and
+                        scrambled it to make a type specimen book. It has
+                        survived not only five centuries, but also the leap into
+                        electronic typesetting, remaining essentially unchanged.
+                        It was popularised in the 1960s with the release of
+                        Letraset sheets containing Lorem Ipsum passages, and
+                        more recently with desktop publishing software like
+                        Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                </div>
             </div>
-
-        
-        </div>
-
-
-
         </Box>
-    )
+    );
 }
