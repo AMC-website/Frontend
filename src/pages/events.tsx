60588a31fe490e4538173f084d21fa61c85f44cd
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2023-05-13 22:28:29 +0530

    fixed error in events.tsx

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -1,58 +1,53 @@
-import EventCard from "@/components/EventCard";
-import { Box, Typography } from "@mui/material"
-import {useTheme} from "@mui/material/styles"
+import EventCard from '@/components/EventCard';
+import { Box, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
-const array = [1,2,3,4,5,6]
+const array = [1, 2, 3, 4, 5, 6];
 
-export default function Events(){
+export default function Events() {
     const theme = useTheme();
     return (
-    <>
-    <Box
-     style={{
-        backgroundColor: 'black',
-        color: "white",
-        justifyContent:"center",
-        alignItems : "center",
-        minHeight: "maxContent"
-       }}
-    >
-        <Box
-        style={{
-            textAlign : "center"
-        }}
-        
-        >
-            <Typography variant="h1">
-               <span
-               style={{
-                color: `${theme.palette.secondary.main}`,
-               }}
-               >E</span> 
-                
-                vents
-            </Typography>
-            <Typography variant="h3">
-                Our latest events
-            </Typography>
-        </Box>
+        <>
+            <Box
+                style={{
+                    backgroundColor: 'black',
+                    color: 'white',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    minHeight: 'maxContent',
+                }}
+            >
+                <Box
+                    style={{
+                        textAlign: 'center',
+                    }}
+                >
+                    <Typography variant="h1">
+                        <span
+                            style={{
+                                color: `${theme.palette.secondary.main}`,
+                            }}
+                        >
+                            E
+                        </span>
+                        vents
+                    </Typography>
+                    <Typography variant="h3">Our latest events</Typography>
+                </Box>
 
-        <Box
-        style={{
-            display : "grid",
-            gridTemplateColumns: "auto auto ",
-            padding : "0 100px",
-            gridGap : "30px"
-        }}
-        >
-          {
-            array.map((item,index)=>
-                <EventCard></EventCard>
-            )
-          }
-          
-        </Box>
-
-    </Box>
-    </>)
+                <Box
+                    style={{
+                        display: 'grid',
+                        gridTemplateColumns: 'auto auto ',
+                        padding: '0 100px',
+                        gridGap: '30px',
+                    }}
+                >
+                    {array.map((item, index) => (
+                        <EventCard key={index + 1}></EventCard>
+                    ))}
+                </Box>
+            </Box>
+        </>
+    );
 }
