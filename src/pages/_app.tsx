6d48d884da69d758379f80c02f8f98c73d501758
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2023-07-24 14:37:31 +0530

    final

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,6 @@ export default function App({ Component, pageProps }: AppProps) {
                 </Box>
 
                 <Component {...pageProps} />
-                <Component {...pageProps} />
 
                 <Box width="100%">
                     <Footer />
