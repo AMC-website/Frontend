9f5e6bd3584d31917ad4b7a337c04575dd43dd56
Harisudarsan <sudarshanhari561@gmail.com>
2023-07-24 13:43:17 +0530

    removed textfield import

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { Button, useMediaQuery } from "@mui/material";
 import Image from "next/image";
