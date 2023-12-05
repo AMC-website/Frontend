c96ee30a5105fee7551596a27aa77e4b20e51b4c
UJJWAL GUPTA <ujjwal.gupta.phy22@itbhu.ac.in>
2023-12-05 12:24:20 +0530

    created a markdown redering page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,20 +5,30 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from '../theme.js';
 import Footer from '@/components/Footer';
 import '@/styles/global.css';
+import { useRouter } from 'next/router';
 export default function App({ Component, pageProps }: AppProps) {
+    let router=useRouter();
+    console.log(router.asPath);
     return (
         <ThemeProvider theme={theme}>
             <Box position="relative" overflow="hidden">
-                <Box bgcolor="black">
+                {/* Checking for pathName used so that in readDoc Feature so that there no nav bar  and footeris shown */}
+                {
+                    router.asPath!=='/readDoc'?<Box bgcolor="black">
                     <Navbar />
-                </Box>
+                </Box>:null
+                }
 
                 <Component {...pageProps} />
                 {/* <Component {...pageProps} /> */}
+                {/* Checking for pathName used so that in readDoc Feature so that there no nav bar  and footeris shown */}
 
-                <Box width="100%">
+                {
+                    router.asPath!=='/readDoc'?<Box width="100%">
                     <Footer />
-                </Box>
+                </Box>:null
+                }
+                
             </Box>
         </ThemeProvider>
     );
