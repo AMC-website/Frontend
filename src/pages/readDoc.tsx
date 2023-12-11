744d34a82d4c141f127b73fc45b030a36d973737
Harisudarsan <97289088+HARI-124@users.noreply.github.com>
2023-12-11 11:37:57 +0530

    Revert "feat: Add type safety to markdown content"

--- src/pages/readDoc.tsx
+++ src/pages/readDoc.tsx
@@ -3,26 +3,28 @@ import React from 'react';
 import { GetStaticProps } from 'next';
 import MarkdownToHtml from 'lib/markDowntoHtml';
 import path from 'path';
-import MarkdownRenderer from './MarkdownRenderer';
+interface MarkdownPageProps {
+  markdownHtml: string;
+}
 
-const MarkdownPage: React.FC<{ markdown: string }> = ({ markdown }) => {
-    return (
-        <div>
-            <h1>Markdown Content</h1>
-            <MarkdownRenderer markdown={markdown} />
-        </div>
-    );
+const MarkdownPage: React.FC<MarkdownPageProps> = ({ markdownHtml }) => {
+  return (
+    <div>
+      <h1>Markdown Content</h1>
+      <div dangerouslySetInnerHTML={{ __html: markdownHtml }} />
+    </div>
+  );
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-    const filePath = path.join(process.cwd(), 'README.md'); // Provide the path to your .md file
-    const markdownHtml = await MarkdownToHtml(filePath);
+  const filePath = path.join(process.cwd(), 'README.md'); // Provide the path to your .md file
+  const markdownHtml = await MarkdownToHtml(filePath);
 
-    return {
-        props: {
-            markdown: markdownHtml,
-        },
-    };
+  return {
+    props: {
+      markdownHtml,
+    },
+  };
 };
 
 export default MarkdownPage;
