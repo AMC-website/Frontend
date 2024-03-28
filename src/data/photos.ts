5a1386bb50a42cd4897a394eebf174ae4238f3e3
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2024-03-28 16:22:25 +0530

    Update Styling on Gallery page, add photo gallery

---
+++ src/data/photos.ts
@@ -0,0 +1,118 @@
+import type { Photo } from 'react-photo-album';
+
+const photos: Photo[] = [
+    {
+        src: '/drone.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/drone2.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight1.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight2.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/drone.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/drone2.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight1.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight2.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/drone.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/drone2.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight1.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight2.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/drone.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/drone2.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight1.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight2.jpg',
+        height: 500,
+        width: 500,
+    },
+];
+const reducedPhotos: Photo[] = [
+    {
+        src: '/drone.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/drone2.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight1.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight2.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight1.jpg',
+        height: 500,
+        width: 500,
+    },
+    {
+        src: '/flight2.jpg',
+        height: 500,
+        width: 500,
+    },
+];
+
+export { photos, reducedPhotos };
--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -1,20 +1,29 @@
-import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 import { Box } from '@mui/material';
 import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
-import Images from '@/data/imagegallery';
+import { PhotoAlbum } from 'react-photo-album';
+import { photos } from '@/data/photos';
+import { bgColor } from '@/constants';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 export default function Gallery() {
+    const breakPoint = useMediaQuery('(min-width:600px)');
+    const breakPoint2 = useMediaQuery('(min-width:1000px)');
     return (
-        <Box>
-            <Box>
+        <Box bgcolor={bgColor} padding="0 7.5% 100px">
+            <Box
+                padding="100px 10px"
+                margin="0 auto"
+                width={breakPoint ? '50%' : '90%'}
+            >
                 <LiteYouTubeEmbed
                     id="L2vS_050c-M"
                     title="What’s new in Material Design for the web (Chrome Dev Summit 2019)"
                 ></LiteYouTubeEmbed>
             </Box>
-            <ImageGallery items={Images} />
+            {/* <ImageGallery items={Images} /> */}
+            <PhotoAlbum layout="columns" photos={photos} padding={5} />
         </Box>
     );
 }
