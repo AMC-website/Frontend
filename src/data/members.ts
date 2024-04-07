5a51ebf783392845ed88e176d277e763f94f93b5
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2024-04-07 15:01:18 +0530

    update members data

--- src/data/members.ts
+++ src/data/members.ts
@@ -2,6 +2,8 @@ export interface MemberDatatype {
     memberName: string;
     memberRole: string;
     memberQuote: string;
+    memberYear?: string;
+    memberImage?: string;
 }
 
 const MemberData: MemberDatatype[] = [
@@ -35,6 +37,101 @@ const MemberData: MemberDatatype[] = [
         memberRole: 'Tech Head',
         memberQuote: 'Empowering through technology.',
     },
+    {
+        memberName: 'Aryan Gupta',
+        memberRole: 'Core Team',
+        memberQuote:
+            "In the vast expanse where sky's dance meets human innovation, aerodynamics becomes the silent conductor, orchestrating our journey beyond earthly limits.",
+    },
+    {
+        memberName: 'Ayush Kumar Dubey',
+        memberRole: 'Core Team',
+        memberQuote:
+            'Amidst the whispers of airflow, dreams take flight, sculpted by the skilled hands of engineers, creating a symphony of motion in the boundless sky.',
+    },
+    {
+        memberName: 'Ayush Rai',
+        memberRole: 'Core Team',
+        memberQuote:
+            'As we delve into the hidden secrets of the sky, each curve and contour unveils a story of exploration and adventure, guided by the principles of aerodynamics.',
+    },
+    {
+        memberName: 'Ayush Verma',
+        memberRole: 'Core Team',
+        memberQuote:
+            "In the seamless harmony of design and science, aerodynamics bridges the gap between imagination and reality, crafting wings that defy gravity's hold.",
+    },
+    {
+        memberName: 'Bhavesh Meghwal',
+        memberRole: 'Core Team',
+        memberQuote:
+            'Navigating the currents of the air, we master the art of flight, charting our course through the ever-changing canvas of the sky with precision and finesse.',
+    },
+    {
+        memberName: 'Chirag Chawla',
+        memberRole: 'Core Team',
+        memberQuote:
+            'In the whispers of the wind lies the poetry of motion, where aerodynamics reveals the dance of science, guiding us through the endless blue horizon.',
+    },
+    {
+        memberName: 'Hardik Goyal',
+        memberRole: 'Core Team',
+        memberQuote:
+            'Silently orchestrating our journey, aerodynamics becomes the maestro of flight, leading us through the vast expanse of the heavens with grace and precision.',
+    },
+    {
+        memberName: 'Harshit Katiyar',
+        memberRole: 'Core Team',
+        memberQuote:
+            'In the language of the skies, aerodynamics speaks volumes, transforming dreams into reality as we soar high above the clouds, propelled by the winds of innovation.',
+    },
+    {
+        memberName: 'Madhav S Baidya',
+        memberRole: 'Core Team',
+        memberQuote:
+            'Defying the laws of gravity, aerodynamics carves paths through the endless blue, as we navigate the boundless expanse of the sky with determination and courage.',
+    },
+    {
+        memberName: 'Om Sharma',
+        memberRole: 'Core Team',
+        memberQuote:
+            'Upon the canvas of the atmosphere, dreams take flight, painted with the brushstrokes of aerodynamics, creating a masterpiece of motion in the vast open sky.',
+    },
+    {
+        memberName: 'Piyush nayak',
+        memberRole: 'Core Team',
+        memberQuote:
+            "In the vast expanse where sky's dance meets human innovation, aerodynamics becomes the silent conductor, orchestrating our journey beyond earthly limits.",
+    },
+    {
+        memberName: 'Pooja Rathod',
+        memberRole: 'Core Team',
+        memberQuote:
+            'Amidst the whispers of airflow, dreams take flight, sculpted by the skilled hands of engineers, creating a symphony of motion in the boundless sky.',
+    },
+    {
+        memberName: 'Sahil Lakhmani',
+        memberRole: 'Core Team',
+        memberQuote:
+            'As we delve into the hidden secrets of the sky, each curve and contour unveils a story of exploration and adventure, guided by the principles of aerodynamics.',
+    },
+    {
+        memberName: 'Vishesh Bairwa',
+        memberRole: 'Core Team',
+        memberQuote:
+            "In the seamless harmony of design and science, aerodynamics bridges the gap between imagination and reality, crafting wings that defy gravity's hold.",
+    },
+    {
+        memberName: 'Chandansumiran Sharma',
+        memberRole: 'Core Team',
+        memberQuote:
+            'Navigating the currents of the air, we master the art of flight, charting our course through the ever-changing canvas of the sky with precision and finesse.',
+    },
+    {
+        memberName: 'Rudradip Mandol',
+        memberRole: 'Core Team',
+        memberQuote:
+            'In the whispers of the wind lies the poetry of motion, where aerodynamics reveals the dance of science, guiding us through the endless blue horizon.',
+    },
 ];
-
 export default MemberData;
