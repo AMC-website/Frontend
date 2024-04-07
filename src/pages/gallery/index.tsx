f66abe8f5c4a20cc8cdd1688daf3e8b155d4e4d1
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2024-04-07 20:14:50 +0530

    Add new photos and update gallery

--- src/pages/gallery/index.tsx
+++ src/pages/gallery/index.tsx
@@ -2,16 +2,13 @@ import { Box, Typography } from '@mui/material';
 import LiteYouTubeEmbed from 'react-lite-youtube-embed';
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 import { PhotoAlbum } from 'react-photo-album';
-import ImageGallery from 'react-image-gallery';
 import { photos } from '@/data/photos';
 import { bgColor, color, h4, h5 } from '@/constants';
 import useMediaQuery from '@mui/material/useMediaQuery';
-import Images from '@/data/imagegallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
 
 export default function Gallery() {
     const breakPoint = useMediaQuery('(min-width:600px)');
-    const breakPoint2 = useMediaQuery('(min-width:1000px)');
     return (
         <Box bgcolor={bgColor} padding="0 7.5% 100px">
             <Box
@@ -39,30 +36,15 @@ export default function Gallery() {
                         textAlign="left"
                         mb="35px"
                     >
-                        Image Gallery{' '}
-                    </Typography>
-                </div>
-                <ImageGallery items={Images} />
-            </Box>
-            <Box
-                color="white"
-                display="flex"
-                flexDirection="column"
-                textAlign="center"
-                marginBottom="6rem"
-            >
-                <div>
-                    <Typography
-                        variant="h1"
-                        fontSize={breakPoint ? h4 : h5}
-                        color={color}
-                        textAlign="left"
-                        mb="35px"
-                    >
                         Photo Album{' '}
                     </Typography>
                 </div>
-                <PhotoAlbum layout="columns" photos={photos} padding={5} />
+                <PhotoAlbum
+                    layout="columns"
+                    columns={3}
+                    photos={photos}
+                    padding={5}
+                />
             </Box>
         </Box>
     );
